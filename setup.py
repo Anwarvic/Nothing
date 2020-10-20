ca7a53d79617919bcacf19caa65c09f48a9ca03b
✌️ Mohamed Anwar <mohamedanwarvic@gmail.com>
2020-10-20 18:59:11 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="nothing",
+    name="nothing_anwarvic",
     version="1.0.0",
     author="Anwarvic",
     author_email="mohamedanwarvic@gmail.com",
