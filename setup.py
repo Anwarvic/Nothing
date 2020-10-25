90591c1b64ee5c0adb5c2213bd3136e022f92ac5
anwarvic <mohamedanwarvic@gmail.com>
2020-10-25 23:42:44 +0200

    updated setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="nothing_anwarvic",
-    version="1.0.1",
+    version="1.0.2",
     author="Anwarvic",
     author_email="mohamedanwarvic@gmail.com",
     description="Nothing.. just nothing!",
